a9af9040503368bc31247c0856659e791781d710
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-03-22 13:14:42 +0300

    Add files via upload

---
+++ test/list.py
@@ -0,0 +1,9 @@
+from tpg import * #inport libs
+
+list=[1,2,3,'test']
+
+#kastcor='>' cursor
+
+out=listgr(list,kastcor='>')
+
+print(out) # print Selected point
---
+++ test/test_yes_or_no.py
@@ -0,0 +1,6 @@
+from tpg import * #inport libs
+
+if yes_or_no('yes or no'): # yes - True no -false 
+    print("press yes") 
+else:
+    print('press no')  
