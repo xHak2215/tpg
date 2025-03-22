0b8041b7e2dafbbea165f5827e4ce6c6e3225f82
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-03-22 13:44:32 +0300

    Add files via upload

---
+++ test/color.py
@@ -0,0 +1,6 @@
+from tpg import * #inport libs
+
+# color stule/стиль,color/цвет,beggraubd/задний фон
+print(color('standart','green','yelou')+'green text,yelou begraund') 
+
+print(color('standart','clear','clear')) # clear colors очистка цветов 
