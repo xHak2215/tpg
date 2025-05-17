a90e1697eaf7a3c2edc18a52400e3ee5efb88f4b
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-05-17 21:41:04 +0300

    Update list.py

--- test/list.py
+++ test/list.py
@@ -4,6 +4,6 @@ list=[1,2,'test','123',]
 
 #kastcor='>' cursor
 
-out=listgr(list,kastcor='>',title='title list',style='scob')
+out=listgr(list,kastcor='>',title='title list',style='scob',ansi=color('green'))
 
 print(out) # print Selected point
