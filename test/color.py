35c736c161ed0a74906b3340f247886552ae8062
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-08-10 14:16:04 +0300

    Update color.py

--- test/color.py
+++ test/color.py
@@ -2,8 +2,7 @@ from tpg import * #inport libs
 import time
 
 # color color/цвет,stule/стиль,beggraubd/задний фон
-print(color('red','standart','yelou')+'green text,yelou begraund') 
-print(color('clear')) # clear colors очистка цветов 
+print(color('green text,yelou begraund','red','standart','yelou')) 
 print('text')
 time.sleep(2)
 clear() # полная очистка и цвет и текст 
