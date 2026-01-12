11f69fc3ae1eb19c0e0cea68ee4405070077d17d
xHak2215 <arikr2886@gmail.com>
2026-01-12 22:10:06 +0300

    улутшение logse

--- tpg/tpg.py
+++ tpg/tpg.py
@@ -711,7 +711,7 @@ class display():
         return strings
         
         
-def write_to_log_file(file, t, path_save_log):
+def write_to_log_file(file, t ,path_save_log):
     if path_save_log:
         path=os.path.join(path_save_log, file)
     else:
@@ -720,12 +720,12 @@ def write_to_log_file(file, t, path_save_log):
         f.write(t+'\n')        
 
 class logse:
-    def __init__(self):
-        self.file='log.log'
-        self.level=2
-        self.seve_log_file=True
+    def __init__(self, file:str='log.log', level:int=2, seve_log_file:bool=True, path_save_log:str|None=None):
+        self.file=file
+        self.level=level
+        self.seve_log_file=seve_log_file
         self.patern=f"{datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")} |{sys._getframe(1).f_locals['__file__']}| "
-        self.path_save_log=None
+        self.path_save_log=path_save_log
         
         # color
         self.color_green='\33[32m'
